357ef6b8c618fb5a203ab720fec10c564a2612f5
ndevreeze <nico@ndv.nl>
2020-07-19 21:19:37 +0200

    Add tests for getting values as strings, values and formatted

--- test/ontodev/excel_test.clj
+++ test/ontodev/excel_test.clj
@@ -14,3 +14,46 @@
   (doall (map check-row data))
   (fact "sheet names" (list-sheets workbook) => (just ["Sheet1" "Foo" "Bar"]))
   (fact "sheet headers" (sheet-headers workbook "Sheet1") => (just ["Format" "Integer" "Float" "Formula"])))
+
+;; wanted to merge this test with the one above, but get failures then.
+(let [workbook (load-workbook "resources/test.xlsx")
+      data     (read-sheet {:values :strings} workbook)]
+  (doall (map check-row data)))
+
+(defn check-row-values
+  [row]
+  (facts "check-row-values"
+    (:integer row) => 1001 
+    (:float row) => 1001.01
+    (:formula row) => 2002.01))
+
+(let [workbook (load-workbook "resources/test.xlsx")
+      data     (read-sheet {:values :values} workbook)]
+  (doall (map check-row-values data)))
+
+(defn check-row-formatted-3
+  [row]
+  (facts "check-row-formatted-3"
+    (:integer row) => "1001.00"
+    (:float row) => "1001.01"
+    (:formula row) => "2002.01"))
+
+(defn check-row-formatted-4
+  [row]
+  (facts "check-row-formatted-4"
+    (:integer row) => "$1,001.00"
+    (:float row) => "$1,001.01"
+    (:formula row) => "$2,002.01"))
+
+(defn check-row-formatted-5
+  [row]
+  (facts "check-row-formatted-5"
+    (:integer row) => "1.00E+03"
+    (:float row) => "1.00E+03"
+    (:formula row) => "2.00E+03"))
+
+(let [workbook (load-workbook "resources/test.xlsx")
+      data     (read-sheet {:values :formatted} workbook)]
+  (check-row-formatted-3 (nth data 3))
+  (check-row-formatted-4 (nth data 4))
+  (check-row-formatted-5 (nth data 5)))
