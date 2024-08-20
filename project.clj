7fcfd7331c0379eaec97dcaf935e5d267cf6f3bc
ndevreeze <nico@ndv.nl>
2024-08-20 20:50:08 +0200

    Add log4j api and core to the project wrt error
    Error was: main ERROR Log4j2 could not find a logging implementation. Please add log4j-core to the classpath. Using SimpleLogger to log to the console

--- project.clj
+++ project.clj
@@ -8,7 +8,13 @@
                  [org.clojure/tools.logging "1.3.0"]
                  ;; 2024-08-17: fixed now, 5.2.5 not needed anymore.
                  [org.apache.poi/poi-ooxml "5.3.0"]
-                 [clojure.java-time "1.4.2"]]
+                 [clojure.java-time "1.4.2"]
+
+                 ;; 2024-08-20: message: ERROR Log4j2 could not find a
+                 ;; logging implementation. Please add log4j-core to
+                 ;; the classpath. This indeed helps.
+                 [org.apache.logging.log4j/log4j-api "2.23.1"]
+                 [org.apache.logging.log4j/log4j-core "2.23.1"]]
   :profiles
   {:dev {:dependencies [[midje "1.10.10"]
                         [lazytest "1.2.3"]]
