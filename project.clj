173e8774998762d318d4794dd7b6876a8981514a
ndevreeze <nico@ndv.nl>
2024-10-19 20:32:42 +0200

    Bump library version(s) wrt lein ancient 2024-10-19

--- project.clj
+++ project.clj
@@ -13,8 +13,8 @@
                  ;; 2024-08-20: message: ERROR Log4j2 could not find a
                  ;; logging implementation. Please add log4j-core to
                  ;; the classpath. This indeed helps.
-                 [org.apache.logging.log4j/log4j-api "2.24.0"]
-                 [org.apache.logging.log4j/log4j-core "2.24.0"]]
+                 [org.apache.logging.log4j/log4j-api "2.24.1"]
+                 [org.apache.logging.log4j/log4j-core "2.24.1"]]
   :profiles
   {:dev {:dependencies [[midje "1.10.10"]
                         [lazytest "1.2.3"]]
